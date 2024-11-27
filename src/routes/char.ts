de5d72e226550483a2bcc12d7a11b38c9990e6ab
唯二 <vear.vip@qq.com>
2024-11-27 09:18:22 +0000

    1

--- src/routes/char.ts
+++ src/routes/char.ts
@@ -1,19 +1,29 @@
 import { Elysia } from 'elysia'
 // @ts-ignore
-import { extractHanzi } from '@vearvip/hanzi-utils';
+import { extractHanzi, queryVariant } from '@vearvip/hanzi-utils';
 import { queryChars } from '../services/char'
 
 export const charRoutes = new Elysia()
   .group('/chars', (app) => app
     .get('/', ({query}) => {
-      console.log('query', query)
+      // console.log('query', query)
       if (!query.q) {
         return new Response('请输入要查询的汉字', { status: 404 })
       }
-      const queryStr = extractHanzi(query.q)
-      console.log('queryStr', queryStr)
-      const chars = queryChars(queryStr)
-      return chars
+      console.log('extractHanzi', extractHanzi(query.q))
+      const chars = extractHanzi(query.q).reduce((ret: string[], val: string) => {
+        const variants = queryVariant(val)
+        // console.log('variants', val, variants)
+        if (variants) {
+          ret.push(val, ...variants)
+        } else {
+          ret.push(val)
+        }
+        return ret
+      }, [])
+      // console.log('chars', chars)
+      const charInfos =  queryChars(chars)
+      return charInfos
     }) 
   )
  
--- src/services/char.ts
+++ src/services/char.ts
@@ -3,11 +3,10 @@ import { HZ } from "../utils/constant";
 
 export function queryChars(charList: string[]) {
   const placeholders = charList.map(char => `'${char}'`).join(', ')
-  console.log('placeholders', placeholders)
-  const sqlStr = `SELECT * FROM mcpdict where ${HZ} IN (${placeholders}) LIMIT 1`
-  console.log('sqlStr', sqlStr)
+  // console.log('placeholders', placeholders)
+  const sqlStr = `SELECT * FROM mcpdict where ${HZ} IN (${placeholders})`
+  // console.log('sqlStr', sqlStr)
   const rows = dbClient.query(sqlStr).all(); 
   return rows
 }
-
 
