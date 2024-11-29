d85befd5f2d24589d911f28d0c6f0431548dd953
唯二 <vear.vip@qq.com>
2024-11-29 04:07:01 +0000

    111

--- src/routes/dialect.ts
+++
@@ -1,5 +0,0 @@
-import { Elysia } from "elysia";
-
-export const dialectRoutes = new Elysia().group("/dialects", (app) =>
-  app.get("/", () => "List dialects").post("/", () => "Create dialect")
-);
