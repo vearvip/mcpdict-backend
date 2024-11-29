737841bcac7cf89b5a27864ea380b1886bd1568e
vear.vip@qq.com <vear.vip@qq.com>
2024-11-30 00:14:35 +0800

    1221

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import charRouter from './routes/char';
 import dialectRouter from './routes/dialect';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 import db from './database';
+import { notFoundMiddleware } from './middlewares/notFoundMiddleware';
 
 const app: Express = express();
 const port: number = 3000;
@@ -16,6 +17,8 @@ app.use(json());
 // 使用中间件解析 URL 编码格式的请求体（例如表单提交）
 app.use(urlencoded({ extended: true }));
 
+// 处理未匹配到的路由
+app.use(notFoundMiddleware);
 // 错误处理中间件
 app.use(errorMiddleware);
 
