6b94751efe523f644b5a35b7b4f8f5d65afeda85
唯二 <vear.vip@qq.com>
2024-11-29 10:14:16 +0000

    23

---
+++ src/database/index.ts
@@ -0,0 +1,7 @@
+import Database from 'better-sqlite3';
+import * as path from 'path';
+ 
+const db = new Database(path.resolve(__dirname, './mcpdict.db'), { verbose: console.log });
+ 
+
+export default db;
