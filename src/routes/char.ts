baa1c4c83c569b4ca4a39dbe4846ed3123b1b39b
vear.vip@qq.com <vear.vip@qq.com>
2024-12-07 20:39:52 +0800

    2134

--- src/routes/char.ts
+++ src/routes/char.ts
@@ -55,13 +55,11 @@ router.get('/', (req: express.Request, res: express.Response) => {
 
   res.send({ 
     data: charInfos,
+    variants: variants,
     success: true
   })
 });
-router.get('/variant', (req: express.Request, res: express.Response) => {
-  // const charList: string[] = req?.body?.char || [];
-  // const dialectList: string[] = req?.body?.dialect || [];
-  // console.log(req?.body)
+router.get('/variant', (req: express.Request, res: express.Response) => { 
   if (!req.query.q) {
     throw new Error('请传入汉字！')
   } else if (typeof req?.query?.q !== 'string') {
@@ -80,56 +78,37 @@ router.get('/variant', (req: express.Request, res: express.Response) => {
 });
 
 router.post('/long', (req: express.Request, res: express.Response) => {
-  const longCharList: string[] = req?.body?.longCharList ?? [];
-  const dialectName: string = req?.body?.dialectName ?? undefined; 
-  // console.log(req?.body)
-  if (longCharList.length === 0) {
+  const charList: string[] = req?.body?.charList ?? [];
+  const dialectName: string = req?.body?.dialectName ?? undefined;  
+  if (charList.length === 0) {
     throw new Error('请传入长文！')
-  } else if (longCharList.length > 1000) {
+  } else if (charList.length > 1000) {
     throw new Error('单次长文注音，不能超过1000个汉字！')
   }  else if (!dialectName) {
     throw new Error('请选择方言以后再进行长文搜索！')
   } 
  
 
-  // const variants = chatService.queryVariants(charList);
-  const variants = longCharList.reduce((acc, char) => {
-    const variant = queryVariant(char);
-    if (variant) {
-      acc.push(...variant);
-    }
-    acc.push(char);
-    return acc;
-  }, [] as string[])
-  // console.log('variants', variants)
-  // console.log('dialectName', dialectName)
-  const chars = chatService.queryChars(variants, [dialectName]);
+  const variants = chatService.queryVariants(charList);  
+  const charRows = chatService.queryChars(variants, [dialectName]);
  
-  const charInfos: any[] = []
-  chars.forEach(charInfo => {
-    const hanZiChar = charInfo[HZ];
-    delete charInfo[HZ];
-    for (const dialectName in charInfo) { 
-      const element = charInfo[dialectName];
-      if (
-        !element
-        || !req.app.locals.dialectNames.includes(dialectName)
-      ) {
-        delete charInfo[dialectName];
-      } 
-    } 
-    charInfos.push({
-      char: hanZiChar,
-      charInfo: charInfo
-    }) 
-  })
+  const charInfoObj = {}
 
+  variants.forEach(char => {
+    const charInfo = charRows.find(charRow => charRow[HZ] === char)
+      // delete charInfo[dialectName];
+      if (charInfo[dialectName]) {
+        charInfoObj[char] = charInfo[dialectName]
+      } 
+  })
   res.send({
-    data: charInfos,
+    data: charInfoObj, 
+    variants: variants,
     success: true
   })
 });
 
+ 
 
  
 
