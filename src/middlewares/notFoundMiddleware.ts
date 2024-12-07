58903e88dac10d41e1fc5acb81043c4cd739028f
vear.vip@qq.com <vear.vip@qq.com>
2024-12-07 16:27:37 +0800

    鞍山市

--- src/middlewares/notFoundMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import express from 'ultimate-express';
-// 创建一个自定义的中间件来包装 res.send 的调用 
-export const notFoundMiddleware = (req, res, next) => {
-  // 设置404状态码和响应信息
-  res.status(404).json({
-    success: false,
-    message: '请请求正确的路由'
-  });
-}
