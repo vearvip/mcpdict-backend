9c3d0ae2de48a88979ae7f05bfc0273a801dcc25
唯二 <vear.vip@qq.com>
2025-01-20 04:33:03 +0000

    新增单个查询方音信息的接口

--- src/routes/dialect.ts
+++ src/routes/dialect.ts
@@ -1,11 +1,61 @@
 import { Elysia } from "elysia";
+import {
+  YuYan,
+  JianCheng,
+ 
+  JingWeiDu,
+
+  DiTuJiErFenQv,
+  DiTuJiErYanSe,
+  DiTuJiErPaiXv,
+
+  YinDianFenQv,
+  YinDianYanSe,
+  YinDianPaiXv,
+
+  ShengDiao,
+
+  Sheng,
+  Shi,
+  Xian,
+  Zhen,
+  Cun,
+  ZiRanCun, 
+} from "../utils/constant";
 
 export const dialectRoutes = new Elysia().group("/dialect", (app) =>
-  app.get("/", ({ store }) => {
-    // let result = store.dialectInfos.map((item) => {
-    //   return Object.values(item);
-    // });
-    // return result
-    return store.dialectInfos
-  })
+  app
+    .get("/", ({ store }) => {
+      // @ts-ignore
+      let result = ((store?.dialectInfos) ?? []).map((item: any) => {
+        let element = {
+          [YuYan]: item[YuYan], // [0] 语言 
+          [JianCheng]: item[JianCheng], // [1] 简称
+          [JingWeiDu]: item[JingWeiDu], // [2] 经纬度
+          [DiTuJiErFenQv]: item[DiTuJiErFenQv], // [3] 地图集二分区
+          [DiTuJiErYanSe]: item[DiTuJiErYanSe], // [4] 地图集二颜色
+          [DiTuJiErPaiXv]: item[DiTuJiErPaiXv], // [5] 地图集二排序
+          [YinDianFenQv]: item[YinDianFenQv], // [6] 音典分区
+          [YinDianYanSe]: item[YinDianYanSe], // [7] 音典颜色
+          [YinDianPaiXv]: item[YinDianPaiXv], // [8] 音典排序
+          [ShengDiao]: JSON.parse(item[ShengDiao]), // [9] 声调
+          [Sheng]: item[Sheng], // [10] 声
+          [Shi]: item[Shi], // [11] 市
+          [Xian]: item[Xian], // [12] 县
+          [Zhen]: item[Zhen], // [13] 镇
+          [Cun]: item[Cun], // [14] 村
+          [ZiRanCun]: item[ZiRanCun], // [15] 自然村
+        }
+        return Object.values(element)
+      })
+
+
+      return result
+    })
+    .get("/item", ({ store, query }) => {
+      // @ts-ignore
+      let result = ((store?.dialectInfos) ?? []).find((item: any) => item[JianCheng] === query.name)
+
+      return result
+    })
 );
--- src/services/dialect.ts
+++ src/services/dialect.ts
@@ -31,7 +31,6 @@ import {
   Zhen,
   Cun,
   ZiRanCun,
-  JieXiRiZhi,
 } from "../utils/constant";
 
 
@@ -43,43 +42,12 @@ export const queryDialectInfos = () => {
 
   const dialectInfos = rows
     .filter((dialectInfoItem: any) => dialectInfoItem[YinDianYanSe])
-    .map((ele: any) => {
-      
-      const item = {
-        [YuYan]: ele[YuYan],
-        [JianCheng]: ele[JianCheng],
-        [LuRuRen]: ele[LuRuRen],
-        [LaiYuan]: ele[LaiYuan],
-        [WenJianMing]: ele[WenJianMing],
-        [BanBen]: ele[BanBen],
-        [ZiShu]: ele[ZiShu],
-        [WuZhengZiShu]: ele[WuZhengZiShu],
-        [YinJieShu]: ele[YinJieShu],
-        [BuDaiDiaoYinJieShu]: ele[BuDaiDiaoYinJieShu],
-        [JingWeiDu]: ele[JingWeiDu],
-        [DiTuJiErFenQv]: ele[DiTuJiErFenQv],
-        [DiTuJiErYanSe]: ele[DiTuJiErYanSe],
-        [DiTuJiErPaiXv]: ele[DiTuJiErPaiXv],
-        [YinDianFenQv]: ele[YinDianFenQv],
-        [YinDianYanSe]: ele[YinDianYanSe],
-        [YinDianPaiXv]: ele[YinDianPaiXv],
-        [ShengDiao]: ele[ShengDiao],
-        [Sheng]: ele[Sheng],
-        [Shi]: ele[Shi],
-        [Xian]: ele[Xian],
-        [Zhen]: ele[Zhen],
-        [Cun]: ele[Cun],
-        [ZiRanCun]: ele[ZiRanCun],
-        // [JieXiRiZhi]: ele[JieXiRiZhi],
-      }
-    
-
-      return item
-    });
+ 
   const dialectNames = dialectInfos.map((dialectInfoItem: any) => dialectInfoItem[JianCheng])
   return {
     dialectInfos,
     dialectNames
   }
 };
+
 
