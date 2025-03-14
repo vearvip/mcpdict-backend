5c86575996c4ce0716c876101d3524e1992560dd
vear.vip@qq.com <vear.vip@qq.com>
2025-03-14 19:54:32 +0800

    关闭cors

--- src/index.ts
+++ src/index.ts
@@ -12,28 +12,30 @@ const app = new Elysia()
   .state({ dialectInfos, dialectNames })
 
 function getDomain(url: string | null) {
-    try {
-        const parsedUrl = new URL(url || '');
-        return parsedUrl.origin; // 返回协议 + 域名，例如 "https://zany-eureka-9qrg9vjvqgjc95rw-3000.app.github.dev"
-    } catch (error: any) {
-        console.error("提供的字符串不是有效的URL:", error.message);
-        return null;
-    }
-}
- 
-app.use(cors({
-  origin: (request: Request) => {
-    const domain = getDomain(request.headers.get('referer')) || ''
-    const vearVipMatchResult = /.*\.vear\.vip$/.test(domain)
-    const githubDevMatchResult = /.*\.github\.dev$/.test(domain)
-    console.log({
-      vearVipMatchResult,
-      githubDevMatchResult,
-      referer:request.headers.get('referer')
-    })
-    // return  vearVipMatchResult || githubDevMatchResult
+  try {
+    const parsedUrl = new URL(url || '');
+    return parsedUrl.origin; // 返回协议 + 域名，例如 "https://zany-eureka-9qrg9vjvqgjc95rw-3000.app.github.dev"
+  } catch (error: any) {
+    console.error("提供的字符串不是有效的URL:", error.message);
+    return null;
   }
-}))
+}
+
+app.use(cors(
+  // {
+  //   origin: (request: Request) => {
+  //     const domain = getDomain(request.headers.get('referer')) || ''
+  //     const vearVipMatchResult = /.*\.vear\.vip$/.test(domain)
+  //     const githubDevMatchResult = /.*\.github\.dev$/.test(domain)
+  //     console.log({
+  //       vearVipMatchResult,
+  //       githubDevMatchResult,
+  //       referer: request.headers.get('referer')
+  //     })
+  //     // return  vearVipMatchResult || githubDevMatchResult
+  //   }
+  // }
+))
 
 // 包装成功返回信息
 app.use(responseFormatterPlugin)
