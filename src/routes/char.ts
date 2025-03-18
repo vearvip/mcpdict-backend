dbd9c9aabaf0a3039368b5b2d7d3d1a2c90ef780
唯二 <vear.vip@qq.com>
2025-03-18 06:31:35 +0000

    调试

--- src/routes/char.ts
+++ src/routes/char.ts
@@ -1,76 +1,115 @@
-import { Elysia, t } from 'elysia';
-import * as chatService from '../services/char';
-import { HanZi } from '../utils/constant';
+import { Elysia, t } from "elysia";
+import * as chatService from "../services/char";
+import { HanZi } from "../utils/constant";
 
 export const charRoutes = new Elysia().group("/char", (app) =>
   app
-    .post('/', ({ body, store }) => {
-      const charList: string[] = (body as any).charList
-      const dialectList: string[] = (body as any).dialectList; 
+    .post("/", ({ body, store }) => {
+      const charList: string[] = (body as any).charList;
+      const dialectList: string[] = (body as any).dialectList;
 
-      if (!charList || !Array.isArray(charList) || (dialectList && !Array.isArray(dialectList)))
-        throw new Error('请传入正确的查询参数');
-      if (charList.length > 10) throw new Error('单次查询，不能超过10个汉字！');
+      if (
+        !charList ||
+        !Array.isArray(charList) ||
+        (dialectList && !Array.isArray(dialectList))
+      )
+        throw new Error("请传入正确的查询参数");
+      if (charList.length > 10) throw new Error("单次查询，不能超过10个汉字！");
       const variants = chatService.queryVariants(charList);
 
       const charRows = chatService.queryChars(variants, dialectList);
-      const charInfos = variants.map(char => {
-        const charInfo = charRows.find(charRow => charRow[HanZi] === char) || {};
+      const charInfos = variants.map((char) => {
+        const charInfo =
+          charRows.find((charRow) => charRow[HanZi] === char) || {};
         delete charInfo[HanZi];
         for (const dialectName in charInfo) {
-          if (!charInfo[dialectName] || !store.dialectNames.includes(dialectName))
+          if (
+            !charInfo[dialectName] ||
+            !store.dialectNames.includes(dialectName)
+          )
             delete charInfo[dialectName];
         }
         return { char, charInfo };
-      })
+      });
 
       return { data: charInfos, variants };
     })
-    .post('/byType', ({ body, store }) => { 
-      const queryStr: string = (body as any).queryStr
-      const dialectList: string[] = (body as any).dialectList;
+    .post("/byType", ({ body, store }) => {
+      const queryStr: string = (body as any).queryStr;
+      let dialectList: string[] = (body as any).dialectList;
+      if (
+        !dialectList ||
+        (Array.isArray(dialectList) && dialectList.length === 0)
+      ) {
+        dialectList = store.dialectNames || [];
+      }
       const queryType: chatService.QueryType = (body as any).queryType;
- 
+      const charRows = chatService.queryCharsByType(
+        queryStr,
+        dialectList,
+        queryType
+      );
 
-      const charRows = chatService.queryCharsByType(queryStr, dialectList, queryType);
- 
-      return { data: charRows };
+      const variants = charRows.map((ele) => ele[HanZi]);
+      const charInfos = variants.map((char) => {
+        const charInfo =
+          charRows.find((charRow) => charRow[HanZi] === char) || {};
+        delete charInfo[HanZi];
+        for (const dialectName in charInfo) {
+          if (
+            !charInfo[dialectName] ||
+            !store.dialectNames.includes(dialectName)
+          )
+            delete charInfo[dialectName];
+        }
+        return { char, charInfo };
+      });
+
+      return { data: charInfos, variants };
     })
-    .get('/variant', ({ query }) => {
-      if (!query.q) throw new Error('请传入汉字！');
-      if (typeof query.q !== 'string') throw new Error('请传入正确的汉字！');
+    .get("/variant", ({ query }) => {
+      if (!query.q) throw new Error("请传入汉字！");
+      if (typeof query.q !== "string") throw new Error("请传入正确的汉字！");
 
-      const charList: string[] = (query.q ?? '').split(',').filter(char => char);
+      const charList: string[] = (query.q ?? "")
+        .split(",")
+        .filter((char) => char);
       const variants = chatService.queryVariants(charList);
 
       return variants;
     })
-    .post('/long', ({ body }) => {
+    .post("/long", ({ body }) => {
       const { charList, dialectName } = body;
 
-      if (charList.length === 0) throw new Error('请传入长文！');
-      if (charList.length > 1000) throw new Error('单次长文注音，不能超过1000个汉字！');
-      if (!dialectName) throw new Error('请选择方言以后再进行长文搜索！');
+      if (charList.length === 0) throw new Error("请传入长文！");
+      if (charList.length > 1000)
+        throw new Error("单次长文注音，不能超过1000个汉字！");
+      if (!dialectName) throw new Error("请选择方言以后再进行长文搜索！");
 
       const variants = chatService.queryVariants(charList);
       const charRows = chatService.queryChars(variants, [dialectName]);
       const charInfoObj = {};
 
-      variants.forEach(char => {
-        const charInfo = charRows.find(charRow => charRow[HanZi] === char) || {};
+      variants.forEach((char) => {
+        const charInfo =
+          charRows.find((charRow) => charRow[HanZi] === char) || {};
         if (charInfo[dialectName]) {
           charInfoObj[char] = charInfo[dialectName];
         }
       });
 
-      return { data: charInfoObj, variants, };
+      return { data: charInfoObj, variants };
     })
-    .post('/info', ({ body }) => {
+    .post("/info", ({ body }) => {
       const { char, infoKeyList } = body;
 
-      if (!char) throw new Error('请传入汉字！');
-      if (!infoKeyList || !Array.isArray(infoKeyList) || infoKeyList.length === 0)
-        throw new Error('请传入需要查询的信息名称！');
+      if (!char) throw new Error("请传入汉字！");
+      if (
+        !infoKeyList ||
+        !Array.isArray(infoKeyList) ||
+        infoKeyList.length === 0
+      )
+        throw new Error("请传入需要查询的信息名称！");
 
       const charRows = chatService.queryCharInfo(char, infoKeyList);
 
--- src/services/char.ts
+++ src/services/char.ts
@@ -2,67 +2,63 @@ import { extractHanzi } from "@vearvip/hanzi-utils";
 import { dbClient } from "../database";
 import { HanZi, YiTiZi } from "../utils/constant";
 
-export type QueryType = 'hanzi' | 'duyin' | 'zhushi' | 'cidian'
+export type QueryType = "hanzi" | "duyin" | "zhushi" | "cidian";
 
 // 查询多个字符的信息
-export function queryChars(charList: string[], dialectList?: string[], queryType: QueryType = 'hanzi'): any[] {
-  const placeholders = charList.join(' OR ');
-  let colStr = '*'
+export function queryChars(
+  charList: string[],
+  dialectList?: string[],
+  queryType: QueryType = "hanzi"
+): any[] {
+  const placeholders = charList.join(" OR ");
+  let colStr = "*";
   if (Array.isArray(dialectList) && dialectList.length > 0) {
-    colStr = [...dialectList, HanZi].map(ele => `\`${ele}\``)?.join(', ')
+    colStr = [...dialectList, HanZi].map((ele) => `\`${ele}\``)?.join(", ");
   }
-  let sqlStr
-  if (queryType == 'hanzi') {
-    sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${HanZi} MATCH '${placeholders}'`
-  } else if (queryType == 'duyin') {
-    sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${HanZi} MATCH '${placeholders}'`
-    sqlStr = `
-      SELECT ${colStr} FROM mcpdict 
-        WHERE ${(dialectList ?? []).map(dialectName => {
-      return `CAST(${dialectName} AS TEXT) LIKE '%${placeholders}%'`
-    }).join(' OR ')
-      }
-    `
-  } else if (queryType == 'zhushi') {
-    sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${HanZi} MATCH '${placeholders}'`
-  } else if (queryType == 'cidian') {
-    sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${HanZi} MATCH '${placeholders}'`
-  }
-  // console.log('sqlStr', sqlStr) 
+  let sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${HanZi} MATCH '${placeholders}'`;
+
+  // console.log("sqlStr ---- ", sqlStr);
   const rows = dbClient.query(sqlStr).all();
   return rows;
 }
 // 查询多个字符的信息
-export function queryCharsByType(queryStr: string, dialectList?: string[], queryType: QueryType = 'hanzi'): any[] {
-
-  let colStr = '*'
+export function queryCharsByType(
+  queryStr: string,
+  dialectList?: string[],
+  queryType: QueryType = "hanzi"
+): any[] {
+  let colStr = "*";
   if (Array.isArray(dialectList) && dialectList.length > 0) {
-    colStr = [...dialectList, HanZi].map(ele => `\`${ele}\``)?.join(', ')
+    colStr = [...dialectList, HanZi].map((ele) => `\`${ele}\``)?.join(", ");
   }
-  let sqlStr = ''
-  if (queryType == 'duyin') {
-    sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${(dialectList ?? []).map(dialectName => {
-      return `${dialectName} MATCH '${queryStr}*'`
-    }).join(' OR ')}`
-    // console.log('sqlStr:', sqlStr)
-  } else if (queryType == 'zhushi') {
-   sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${(dialectList ?? []).map(dialectName => {
-      return `${dialectName} MATCH '${queryStr}'`
-    }).join(' OR ')}`
-    console.log('sqlStr:', sqlStr)
-  } else if (queryType == 'cidian') {
-    // sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${HanZi} MATCH '${queryStr}'`
+  let sqlStr = "";
+  if (queryType == "duyin") {
+    sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${(dialectList ?? [])
+      .map((dialectName) => {
+        return `${dialectName} MATCH '${queryStr}*'`;
+      })
+      .join(" OR ")}`;
+    console.log("sqlStr:", sqlStr);
+  } else if (queryType == "zhushi") {
+    sqlStr = `SELECT ${colStr} FROM mcpdict WHERE ${(dialectList ?? [])
+      .map((dialectName) => {
+        return `${dialectName} MATCH '${queryStr}'`;
+      })
+      .join(" OR ")}`;
+    console.log("sqlStr:", sqlStr);
+  } else if (queryType == "cidian") {
+    // sqlStr = `SELECT * FROM mcpdict WHERE 普通话 MATCH 'dao1'`
   }
-  // console.log('sqlStr', sqlStr) 
+  // console.log('sqlStr', sqlStr)
   const rows = dbClient.query(sqlStr).all();
   return rows;
 }
 
 // 查询多个字符的信息
 export function queryCharInfo(char: string, infoKeyList: string[]): any[] {
-  let colStr = '*'
+  let colStr = "*";
   if (Array.isArray(infoKeyList) && infoKeyList.length > 0) {
-    colStr = [...infoKeyList, HanZi].map(ele => `\`${ele}\``)?.join(', ')
+    colStr = [...infoKeyList, HanZi].map((ele) => `\`${ele}\``)?.join(", ");
   }
   const sqlStr = `SELECT ${colStr}, ${HanZi} FROM mcpdict WHERE ${HanZi} MATCH '${char}'`;
   // console.log('sqlStr', sqlStr)
@@ -70,12 +66,10 @@ export function queryCharInfo(char: string, infoKeyList: string[]): any[] {
   return rows;
 }
 
-
-
 // 查询多个字符的变体
 export function queryVariants(charList: string[]): string[] {
-  const hanziCharList = extractHanzi(charList.join('')); // 提取字符串中的汉字
-  const placeholders = hanziCharList.join(' OR ');
+  const hanziCharList = extractHanzi(charList.join("")); // 提取字符串中的汉字
+  const placeholders = hanziCharList.join(" OR ");
   const sqlStr = `SELECT ${HanZi}, ${YiTiZi} FROM mcpdict WHERE ${HanZi} MATCH '${placeholders}'`;
   const rows = dbClient.query(sqlStr).all();
 
@@ -85,8 +79,8 @@ export function queryVariants(charList: string[]): string[] {
   // 遍历查询结果，将每个字符及其变体添加到变体数组中
   rows.forEach((row: any) => {
     variants.push(row[HanZi]); // 添加当前字符
-    if (typeof row[YiTiZi] === 'string') {
-      variants.push(...row[YiTiZi].split(' ')); // 添加变体
+    if (typeof row[YiTiZi] === "string") {
+      variants.push(...row[YiTiZi].split(" ")); // 添加变体
     }
   });
 
