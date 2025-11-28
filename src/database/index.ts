c923b043c0f13bc68cea33dd8bb59c71d36ea6bb
唯二 <vear.vip@qq.com>
2025-11-28 16:20:33 +0800

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,4 @@
-
 // @ts-ignore
-// import dbClient from "./mcpdict.db" with { "type": "sqlite" };
-// @ts-ignore
-import dbClient from "./sub_mcpdict.db" with { "type": "sqlite" };
-
+import dbClient from "./mcpdict.db" with { "type": "sqlite" }; 
 
 export { dbClient };
